fd112ba7cc55c1188dc7115e9178e45d24d7e929
Snowhite3105 <83868245+Snowhite3105@users.noreply.github.com>
2022-04-06 10:00:43 -0400

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -36,5 +36,19 @@ question_turn = "player1";
 answer_turn = "player2";
 
 function check(){
-    
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("answer in lower case - " + answer;
+                if (answer == word);
+    if (answer_turn == "player1");
+    player1_score = player1_score +1
+    document.getElementById("player1_score").innerHTML = player1_score;
+                            }
+else
+{
+ player2_score = player2_score +1
+    document.getElementById("player2_score").innerHTML = player2_score;
 }
+}
+if(question_turn == "player1")
+   
